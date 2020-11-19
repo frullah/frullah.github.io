18809508bd3485038ee4856218bfcfc2ed189a82
Fajarullah <frullah12@gmail.com>
2020-11-20 03:44:00 +0700

    replace scss with postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,18 @@
 const plugins = [
-  require('postcss-import'),
+  require('postcss-nested'),
   require('tailwindcss')('./tailwind.config.js'),
   require('autoprefixer')
 ]
 
 if (process.env.NODE_ENV === 'production') {
-  const cssnano = require('cssnano')
+  const cssnano = require('cssnano')({
+    preset: [
+      'default',
+      {
+        discardComments: { removeAll: true }
+      }
+    ]
+  })
   const purgecss = require('@fullhuman/postcss-purgecss')({
     content: ['./src/**/*.svelte', './node_modules/tippy.js/dist/tippy.css'],
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
