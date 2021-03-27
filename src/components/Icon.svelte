0c9d6179689d9ab8a4461017ff0f457cc15b858b
frullah <frullah12@gmail.com>
2021-03-27 18:07:36 +0700

    add icon component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let name;
+  $$restProps.class = `icon ${$$restProps.class || ""}`
+</script>
+
+<style global lang="scss">
+  .icon {
+    width: 1em;
+    height: 1em;
+    display: inline-block;
+    overflow: visible;
+    fill: currentColor;
+  }
+</style>
+
+<svg {...$$restProps}>
+  <use href="#{name}"></use>
+</svg>
