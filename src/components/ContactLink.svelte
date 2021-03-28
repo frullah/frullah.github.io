8b094418c5b98dbfdf0a915cef3b071e13b3890e
frullah <frullah12@gmail.com>
2021-03-28 14:36:07 +0700

    update home contents

---
+++ src/components/ContactLink.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Icon from "./Icon.svelte";
+  import tippy from "sveltejs-tippy";
+  import "tippy.js/dist/svg-arrow.css";
+  export let content;
+  export let href;
+  export let name;
+  const tippyProps = {
+    placement: "bottom",
+    interactive: true,
+    content: content,
+  };
+</script>
+
+<a {href} rel="noopener" target="_blank" use:tippy={tippyProps}>
+  <Icon class="text-[40px] {name}" {name} />
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,9 @@
+<script>
+  import HomeSection from "../components/Sections/HomeSection.svelte";
+</script>
+
 <svelte:head>
   <title>Fajarullah</title>
 </svelte:head>
 
-<section id="home" class="container max-w-screen-sm mx-auto mt-24 p-4">
-  <h1 class="text-2xl text-center mb-4">Hi, I am <strong>Fajarullah</strong></h1>
-	<p class="text-center mb-4">
-		I am a Full Stack Engineer, I love to build web stuff. I often use
-		<a
-			class="text-primary-600"
-			rel="noopener"
-			target="_blank"
-			href="https://www.ruby-lang.org">Ruby</a>, and
-		<a
-			class="text-primary-600"
-			rel="noopener"
-			target="_blank"
-			href="https://developer.mozilla.org/docs/Web/JavaScript">JavaScript</a>
-		as my programming languages. When I have free time, sometime i learn about machine learning, playing
-		<a
-			class="text-primary-600"
-			rel="noopener"
-			target="_blank"
-			href="https://osu.ppy.sh/users/5132441">osu!</a>, watching animes, or
-		learn foreign languages.
-	</p>
-</section>
+<HomeSection />
