aff74209734d98432e0ce2f3c6a542e1449e08d8
frullah <frullah12@gmail.com>
2021-03-28 14:54:10 +0700

    set tech stacks section max width to screen-sm

--- src/components/Sections/TechnologiesSection.svelte
+++ src/components/Sections/TechnologiesSection.svelte
@@ -23,7 +23,7 @@
 </script>
 
 <section id="technologies">
-  <div class="container my-4 p-4">
+  <div class="container my-4 p-4 max-w-screen-sm">
     <h2 class="text-xl text-center mb-4">My Tech Stacks</h2>
     <ul class="flex flex-wrap justify-center">
       {#each usedTechnologies as usedTechnology}
