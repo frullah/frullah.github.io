29e138698fe1eebb2d1c1456b81d2d6594d6995c
frullah <frullah12@gmail.com>
2021-05-21 14:01:29 +0700

    remove navigation

--- src/components/Layout/Nav.svelte
+++ src/components/Layout/Nav.svelte
@@ -21,49 +21,5 @@
       class:hidden={!navState}
       class="fixed inset-0 z-10 bg-gray-900 opacity-50 sm:hidden hidden"
     />
-
-    <Button class="m-0 ml-2 block sm:hidden" on:click={() => (navState = true)}>
-      <Icon class="text-xl" name="bars" />
-    </Button>
-
-    <nav class:open={navState}>
-      <Button class="sm:hidden block ml-auto" on:click={closeNav}>
-        <Icon class="text-2xl" name="times" />
-      </Button>
-      <ul>
-        {#each links as link}
-          <li><a on:click={closeNav} href={link.href}>{link.text}</a></li>
-        {/each}
-      </ul>
-    </nav>
   </div>
 </header>
-
-<style lang="scss">
-  nav {
-    @apply fixed
-      sm:static
-      inset-y-0
-      right-0
-      sm:w-auto
-      w-[256px]
-      transform
-      translate-x-full
-      sm:translate-x-0
-      transition
-      duration-200
-      sm:block
-      bg-white
-      z-20;
-
-    &.open {
-      @apply block
-        translate-x-0
-        ease-out;
-    }
-
-    & ul li a {
-      @apply block py-2.5 px-4 font-medium hover:text-primary-600;
-    }
-  }
-</style>
