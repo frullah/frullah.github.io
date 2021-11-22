4a5376ed3ae946eda9ac9fc9b51554de5f2f23fc
Fajarullah <frullah12@gmail.com>
2021-11-22 21:44:42 +0700

    remove useless nav element

--- src/components/Layout/Nav.svelte
+++ src/components/Layout/Nav.svelte
@@ -1,21 +1,9 @@
-<script>
-  let navState = false;
 
-  function closeNav() {
-    navState = false;
-  }
-</script>
 
 <header
   class="flex items-center py-1 px-4 border-b-[3px] h-[54px] z-20 fixed inset-x-0 top-0 bg-white"
 >
   <div class="container max-w-screen-sm flex items-center">
     <a class="text-2xl" href="#home">Fajarullah</a>
-    <div class="flex-grow" />
-    <div
-      on:click={closeNav}
-      class:hidden={!navState}
-      class="fixed inset-0 z-10 bg-gray-900 opacity-50 sm:hidden hidden"
-    />
   </div>
 </header>
